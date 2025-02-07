82dc3fde458efb48ef0c46097b59a2977b2b4027
Zebraloew <the.vhor@gmail.com>
2025-02-07 19:54:38 +0100

    menu working in public page

--- public/nav.js
+++ public/nav.js
@@ -2,17 +2,20 @@ export function createNavbar() {
     const nav = document.createElement("nav");
     nav.className = "navbar";
     nav.innerHTML = `
-        <h1>Klubzmer</h1>
-        <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-        <div class="burger" id="burger">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+        <nav class="navbar">
+    <input type="checkbox" id="menu-toggle" class="menu-toggle">
+    <label for="menu-toggle" class="menu-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+    <ul class="menu">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
     `;
 
     document.body.prepend(nav);
