55d32e7f3ec949c40418df73778aa6027a178b94
Zebraloew <the.vhor@gmail.com>
2025-02-09 14:49:00 +0100

    still working on the wave

--- public/modules/section-img-contact.js
+++ public/modules/section-img-contact.js
@@ -3,10 +3,8 @@ export function createSectionImgContact() {
     sectionImgContact.className = "container";
     sectionImgContact.id = "img-container-1";
     sectionImgContact.innerHTML = `
-    <section class="section-img" id="section-img-1">
-        <div class="wavy-background"></div>
-        <img class="" src="img/hintereinander_j.jpg" alt="Gruppenfoto">
-    </section>
+        <section class="section-img" id="section-img-1">
+            <img class="" src="img/hintereinander_j.jpg" alt="Gruppenfoto">
     `;
 
     // Correctly append the section to the <main> element
