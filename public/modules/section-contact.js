cbdd88a9b86d1986bf18e5ae41606bc7eaff63bb
Zebraloew <the.vhor@gmail.com>
2025-02-09 12:22:14 +0100

    visual tweaks CSS — Typography

--- public/modules/section-contact.js
+++ public/modules/section-contact.js
@@ -7,10 +7,9 @@ export function createSectionContact() {
         <img class="head-img" src="img/hintereinander.jpg" alt="Gruppenfoto">
         <div class="textwrapper">
             <h2>Kontakt</h2>
-            <p>
-                 Wer uns für eine Geburtstagsfeier, eine Hochzeit, einen Gig im eigenen Wohnzimmer oder auf großer Bühne, drinnen oder draußen (unplugged oder verstärkt) buchen möchte, schreibt uns am besten eine Nachricht an:
-            <br><br>  <a href="mailto:info@klubzmer.de">info@klubzmer.de</a>
-            <br><br> Aktuelles zu Gigs und andere Neuigkeiten gibt es auch auf unserer 
+            <p> Wer uns für eine Geburtstagsfeier, eine Hochzeit, einen Gig im eigenen Wohnzimmer oder auf großer Bühne, drinnen oder draußen (unplugged oder verstärkt) buchen möchte, schreibt uns am besten eine Nachricht an:</p>
+            <a id="main-contact-link" href="mailto:info@klubzmer.de">info<span class="atsign">@</span>klubzmer.de</a>
+            <p>Aktuelles zu Gigs und andere Neuigkeiten gibt es auch auf unserer 
             <br> <a href="https://www.facebook.com/hamburgerklubzmer/" target="_blank" rel="noopener noreferrer">Facebook-Seite!</a>
             </p>
         </div>
