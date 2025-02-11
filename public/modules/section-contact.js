b6827ee49b8cd8ba2a134b76cad955fadef3143e
Zebraloew <the.vhor@gmail.com>
2025-02-11 22:37:50 +0100

    load text integrated into main part. now the contact text can be edited in the admin area

--- public/modules/section-contact.js
+++ public/modules/section-contact.js
@@ -1,3 +1,5 @@
+import { loadText } from "../textLoader.js";
+
 export function createSectionContact() {
     const sectionContact = document.createElement("div");
     sectionContact.className = "container";
@@ -7,7 +9,7 @@ export function createSectionContact() {
         <!-- <img class="head-img" src="img/hintereinander.jpg" alt="Gruppenfoto"> -->
         <div class="textwrapper">
             <h2>Kontakt</h2>
-            <p> Wer uns für eine Geburtstagsfeier, eine Hochzeit, einen Gig im eigenen Wohnzimmer oder auf großer Bühne, drinnen oder draußen (unplugged oder verstärkt) buchen möchte, schreibt uns am besten eine Nachricht an:</p>
+            <p id="contact-content"> Wer uns für eine Geburtstagsfeier, eine Hochzeit, einen Gig im eigenen Wohnzimmer oder auf großer Bühne, drinnen oder draußen (unplugged oder verstärkt) buchen möchte, schreibt uns am besten eine Nachricht an:</p>
             <a id="main-contact-link" href="mailto:info@klubzmer.de">info<span class="atsign">@</span>klubzmer.de</a>
             <p>Aktuelles zu Gigs und andere Neuigkeiten gibt es auch auf unserer 
             <br> <a href="https://www.facebook.com/hamburgerklubzmer/" target="_blank" rel="noopener noreferrer">Facebook-Seite!</a>
@@ -23,4 +25,5 @@ export function createSectionContact() {
 
     // Correctly append the section to the <main> element
     document.querySelector('main').append(sectionContact);
+    loadText("contact.txt", "contact-content");
 }
